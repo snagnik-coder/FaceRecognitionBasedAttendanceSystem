9d179ecd245dea3067bc412e2ba8bbb533d3eb4d
Snagnik Das <67196943+snagnik-coder@users.noreply.github.com>
2023-08-17 15:19:46 +0530

    Added web files

---
+++ static/index.js
@@ -0,0 +1,7 @@
+// const logo = document.getElementById("logo");
+// const attend = document.getElementById("attend");
+// const register = document.getElementById("register");
+
+// attend.addEventListener("hover", (e)=>{
+//     console.log("hover");
+// });
